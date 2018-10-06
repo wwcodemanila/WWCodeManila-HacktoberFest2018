d57bbb0065d40cd7ef41d73fa18562c268901784
Kim Morano <ksmorano@gmail.com>
2018-10-06 15:45:59 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      author: "",
+      author_url: "",
+      img: "",
+      post_url: ""
+    };
+  }
+
+  componentDidMount() {
+    axios.get("https://picsum.photos/list").then(response => {
+      const data = response.data;
+      const random = Math.floor(Math.random() * data.length);
+      this.setState({
+        author: data[random].author,
+        author_url: data[random].author_url,
+        img: `https://picsum.photos/1080/720?image=${data[random].id}`,
+        post_url: data[random].post_url
+      });
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <div className="background">
+          <img
+            src={this.state.img}
+            alt={this.state.post_url}
+            width="100%"
+            height="100%"
+          />
+        </div>
+      </React.Fragment>
     );
   }
 }
